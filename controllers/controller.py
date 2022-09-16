4d15225680fdc9aa739489128332acbd76f25b9b
Cammy Smith <cjsarab1991@gmail.com>
2022-09-16 14:43:48 +0100

    Made some changes to wiki links

--- controllers/controller.py
+++ controllers/controller.py
@@ -23,8 +23,18 @@ def add_book():
     book_author_list = book_author.split()
     author_first = str(book_author_list[0])
     author_second = str(book_author_list[1])
-    book_author_link =  author_first + "_" + author_second
 
+    if len(book_author_list) == 3:
+        author_third = str(book_author_list[2])
+        book_author_link = author_first + "_" + author_second + "_" + author_third 
+
+    elif len(book_author_list) == 4:
+        author_fourth = str(book_author_list[3])
+        book_author_link = author_first + "_" + author_second + "_" + author_third + "_" + author_fourth
+
+    else:
+        book_author_link = author_first + "_" + author_second
+        
     new_book = Book(book_title, book_author, book_genre, book_checked_out, book_return_date, book_author_link)
 
     add_new_book(new_book)
